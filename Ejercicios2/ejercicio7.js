5ba63477e6dc61c96bca45f9a408f1db15e8ce8e
camila navas <navascamila129@gmail.com>
2024-03-20 13:25:27 -0500

    proof

--- Ejercicios2/ejercicio7.js
+++ Ejercicios2/ejercicio7.js
@@ -1,10 +1,14 @@
 // Crea una función que tome una cadena como parámetro y devuelva la cadena en orden
 // inverso, por ejemplo, si la cadena de entrada es ‘Hola’, la función debería retornar ‘aloH’.
+console.log("asd")
 let palabra = prompt("INGRESE LA PALABRA A REVERTIR")
-let reverse = '';
-function reverse (palabra){
-    for (let i = palabra.length[-1]; i >= 0; i--) {
-        reverse += pa
-        
+
+function revers(palabra){
+    let reverse = '';
+    for (let i = palabra.length -1; i >= 0; i--) {
+        reverse += palabra[i];
     }
+    return reverse;
 }
+alert(revers(palabra))
+// let mostrar = reverse
